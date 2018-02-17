1bac080cef9a7cdb7377a3b052dd4758b9780b15
Michael Zhang <michael13162@gmail.com>
2018-02-17 01:46:29 -0800

    Implemented upload and project.

--- model/gen.py
+++ model/gen.py
@@ -7,19 +7,18 @@ Created on Fri Feb 16 20:29:55 2018
 """
 
 from model.data import preprocessing
-from model import nn
+from model.model import nn
 
-# Load data from small data set
-X_train, y_train, X_test, y_test = preprocessing.load_small_dataset()
 
-# Create & load model
-model = nn(X_train, y_train, X_test, y_test)
-model.load()
+def gen(target_molecules):
+    # Load data from small data set
+    X_train, y_train, X_test, y_test = preprocessing.load_small_dataset()
 
-# Molecules to use as a seed for generating
-target_molecules = ['NC=NC1CN1CO', 'CC1=CNCN2CC12']
+    # Create & load model
+    model = nn(X_train, y_train, X_test, y_test)
+    model.load()
 
-# Generate a molecule
-molecules = model.generate(target=target_molecules)
+    # Generate a molecule
+    molecules = model.generate(target=target_molecules)
 
-# Parse molecules to json...?
+    return molecules
