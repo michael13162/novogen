11f34dba8151d2e84eb2251b45425eade9b548ff
Michael Zhang <michael13162@gmail.com>
2018-02-18 05:12:38 +0000

    sdlfkj

--- backend.py
+++ backend.py
@@ -1,14 +1,24 @@
-from flask import Flask, render_template, request, json
+from flask import Flask, render_template, request, json, redirect
 from pymongo import MongoClient
 from model.gen import gen
 import random
 import base64
 import cStringIO
 from elasticsearch import Elasticsearch
+from urllib import unquote
 
 es = Elasticsearch()
 app = Flask(__name__)
 
+j = []
+a = 0
+
+class safe:
+    def set(self, values):
+        self.values = values
+        return
+
+my_safe = safe()
 
 @app.route('/')
 def hello_world():
@@ -21,18 +31,17 @@ def search():
 
 @app.route('/home')
 def home():
-    data = []
-    for i in range(0, 100):
-        data.append({
-            'molecule': 'Name ' + str(i + 1),
-            'logP': round(random.random(), 2),
-            'TPSA': round(random.random(), 2),
-            'molWeight': random.random() * 500,
-            'donors': int(random.random() * 5),
-            'acceptors': int(random.random() * 4),
-            'image': 'R0lGODlhPQBEAPeoAJosM//AwO/AwHVYZ/z595kzAP/s7P+goOXMv8+fhw/v739/f+8PD98fH/8mJl+fn/9ZWb8/PzWlwv///6wWGbImAPgTEMImIN9gUFCEm/gDALULDN8PAD6atYdCTX9gUNKlj8wZAKUsAOzZz+UMAOsJAP/Z2ccMDA8PD/95eX5NWvsJCOVNQPtfX/8zM8+QePLl38MGBr8JCP+zs9myn/8GBqwpAP/GxgwJCPny78lzYLgjAJ8vAP9fX/+MjMUcAN8zM/9wcM8ZGcATEL+QePdZWf/29uc/P9cmJu9MTDImIN+/r7+/vz8/P8VNQGNugV8AAF9fX8swMNgTAFlDOICAgPNSUnNWSMQ5MBAQEJE3QPIGAM9AQMqGcG9vb6MhJsEdGM8vLx8fH98AANIWAMuQeL8fABkTEPPQ0OM5OSYdGFl5jo+Pj/+pqcsTE78wMFNGQLYmID4dGPvd3UBAQJmTkP+8vH9QUK+vr8ZWSHpzcJMmILdwcLOGcHRQUHxwcK9PT9DQ0O/v70w5MLypoG8wKOuwsP/g4P/Q0IcwKEswKMl8aJ9fX2xjdOtGRs/Pz+Dg4GImIP8gIH0sKEAwKKmTiKZ8aB/f39Wsl+LFt8dgUE9PT5x5aHBwcP+AgP+WltdgYMyZfyywz78AAAAAAAD///8AAP9mZv///wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAEAAKgALAAAAAA9AEQAAAj/AFEJHEiwoMGDCBMqXMiwocAbBww4nEhxoYkUpzJGrMixogkfGUNqlNixJEIDB0SqHGmyJSojM1bKZOmyop0gM3Oe2liTISKMOoPy7GnwY9CjIYcSRYm0aVKSLmE6nfq05QycVLPuhDrxBlCtYJUqNAq2bNWEBj6ZXRuyxZyDRtqwnXvkhACDV+euTeJm1Ki7A73qNWtFiF+/gA95Gly2CJLDhwEHMOUAAuOpLYDEgBxZ4GRTlC1fDnpkM+fOqD6DDj1aZpITp0dtGCDhr+fVuCu3zlg49ijaokTZTo27uG7Gjn2P+hI8+PDPERoUB318bWbfAJ5sUNFcuGRTYUqV/3ogfXp1rWlMc6awJjiAAd2fm4ogXjz56aypOoIde4OE5u/F9x199dlXnnGiHZWEYbGpsAEA3QXYnHwEFliKAgswgJ8LPeiUXGwedCAKABACCN+EA1pYIIYaFlcDhytd51sGAJbo3onOpajiihlO92KHGaUXGwWjUBChjSPiWJuOO/LYIm4v1tXfE6J4gCSJEZ7YgRYUNrkji9P55sF/ogxw5ZkSqIDaZBV6aSGYq/lGZplndkckZ98xoICbTcIJGQAZcNmdmUc210hs35nCyJ58fgmIKX5RQGOZowxaZwYA+JaoKQwswGijBV4C6SiTUmpphMspJx9unX4KaimjDv9aaXOEBteBqmuuxgEHoLX6Kqx+yXqqBANsgCtit4FWQAEkrNbpq7HSOmtwag5w57GrmlJBASEU18ADjUYb3ADTinIttsgSB1oJFfA63bduimuqKB1keqwUhoCSK374wbujvOSu4QG6UvxBRydcpKsav++Ca6G8A6Pr1x2kVMyHwsVxUALDq/krnrhPSOzXG1lUTIoffqGR7Goi2MAxbv6O2kEG56I7CSlRsEFKFVyovDJoIRTg7sugNRDGqCJzJgcKE0ywc0ELm6KBCCJo8DIPFeCWNGcyqNFE06ToAfV0HBRgxsvLThHn1oddQMrXj5DyAQgjEHSAJMWZwS3HPxT/QMbabI/iBCliMLEJKX2EEkomBAUCxRi42VDADxyTYDVogV+wSChqmKxEKCDAYFDFj4OmwbY7bDGdBhtrnTQYOigeChUmc1K3QTnAUfEgGFgAWt88hKA6aCRIXhxnQ1yg3BCayK44EWdkUQcBByEQChFXfCB776aQsG0BIlQgQgE8qO26X1h8cEUep8ngRBnOy74E9QgRgEAC8SvOfQkh7FDBDmS43PmGoIiKUUEGkMEC/PJHgxw0xH74yx/3XnaYRJgMB8obxQW6kL9QYEJ0FIFgByfIL7/IQAlvQwEpnAC7DtLNJCKUoO/w45c44GwCXiAFB/OXAATQryUxdN4LfFiwgjCNYg+kYMIEFkCKDs6PKAIJouyGWMS1FSKJOMRB/BoIxYJIUXFUxNwoIkEKPAgCBZSQHQ1A2EWDfDEUVLyADj5AChSIQW6gu10bE/JG2VnCZGfo4R4d0sdQoBAHhPjhIB94v/wRoRKQWGRHgrhGSQJxCS+0pCZbEhAAOw=='
-        })
-    return render_template('home.html', projects=data)
+    print('111111')
+    #json1 = request.args.get('json')
+#    print(request.query_string)
+    #json1 =json.loads(unquote(request.query_string.partition('&')[0])) 
+    #json1 = unquote(request.query_string.partition('&')[0])  
+    #print('2222222')
+    #print(json1)
+    print(my_safe.values)
+    print(a)
+    #obj = json.loads(json1)
+    return render_template('home.html', projects=my_safe.values['json'])
 
 @app.route('/upload')
 def uploadFile():
@@ -105,7 +114,7 @@ def user():
     for pid in u['projects']:
         p = projects.find_one({'project': pid})
         res[pid] = p['name']
-
+    j = res
     response = app.response_class(
         response=json.dumps(res),
         status=200,
@@ -155,12 +164,11 @@ def project():
 
 @app.route('/api/project/upload', methods=['POST'])
 def upload():
-    json = request.get_json(force=True)
-    lines = json['file']
+    lines = request.get_json(force=True)['file']
     print(lines)
     molecules = gen(lines)
 
-    res = []
+    res = {'json': []}
     for m in molecules:
         buffered = cStringIO.StringIO()
         m.molecular_img.save(buffered, format='PNG')
@@ -171,9 +179,9 @@ def upload():
                     'num_h_donors': m.num_h_donors,
                     'num_h_acceptors': m.num_h_acceptors,
                     'molecular_weight': m.molecular_weight,
-                    'molecular_img': img_str}
+                    'molecular_img': 'a'}
 
-        res.append(molecule)
+        res['json'].append(molecule)
 
     '''
     projects = mongo_projects()
@@ -198,9 +206,18 @@ def upload():
         }
     )
     '''
+    j = res
+    a = 11
+    print(j)
+    my_safe.values = res
+    print('1112123')
+    response = app.response_class(
+        response=json.dumps(res),
+        status=200,
+        mimetype='application/json'
+    )
 
-    print(res)
-    return render_template('home.html', summary = json.dumps(res))
+    return response
 
 
 if __name__ == '__main__':
