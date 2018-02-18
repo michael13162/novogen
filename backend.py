ed4b9aad1177206ffdaa34789ab68106afbc5662
Aram Baghdassarian <abagh0703@gmail.com>
2018-02-18 08:47:32 +0000

    fixe dport

--- backend.py
+++ backend.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, json, redirect
 from pymongo import MongoClient
-# from model.gen import gen
+from model.gen import gen
 import random
 import base64
 import cStringIO
@@ -223,4 +223,4 @@ def perform_query():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=80)
